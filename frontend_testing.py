6a6434e0ac25c18f079f6999c7f2ba673afee9cb
zrihennoy <zrihennoy>
2023-05-14 20:32:50 +0300

    test

--- frontend_testing.py
+++ frontend_testing.py
@@ -2,7 +2,7 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-try:
+try:-
     driver = webdriver.Chrome()
     driver.get("http://127.0.0.1:5001/users/get_user_data/1")
     elem = driver.find_element(By.TAG_NAME,"H1")

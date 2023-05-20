830e2f11448e15aa12bbd6c663fbbc88648ef44d
zrihennoy <zrihennoy@gmail.com>
2023-05-20 20:37:56 +0300

    testconflict3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+test2
 
 # Press the green button in the gutter to run the script.
 
